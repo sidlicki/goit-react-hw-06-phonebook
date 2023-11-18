964e9dff1fa4a5393f1c46bc371f9fb452f82fb4
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-11-18 16:37:26 +0100

    Update contacts.reducer.js

--- src/redux/contacts/contacts.reducer.js
+++ src/redux/contacts/contacts.reducer.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const exampleConacts = [
   { id: 'id-1', name: 'Joe Biden', number: '+1-800-375-5283' },
   { id: 'id-2', name: 'Naruhito 徳仁', number: '+81-354-74-97-70' },
-  { id: 'id-3', name: 'Frank-Walter Steinmeier', number: '+49-30-28-88-71-28' },
+  { id: 'id-3', name: 'Frank Steinmeier', number: '+49-30-28-88-71-28' },
   { id: 'id-4', name: 'Rishi Sunak', number: ' +44-0-131-556-0023' },
   { id: 'id-5', name: 'Emmanuel Macron', number: '+33-1-43-06-07-37' },
   { id: 'id-6', name: 'Sergio Mattarella', number: '+39-06-8200-3641' },
